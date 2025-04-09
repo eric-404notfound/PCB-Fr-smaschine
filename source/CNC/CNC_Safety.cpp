32acf59f133dc09f6331d09c3568e29604bdc786
Sallwuerk <marcleuze@outlook.de>
2025-04-09 11:35:58 +0200

    Update CNC_Safety.cpp
    Kommentare

--- source/CNC/CNC_Safety.cpp
+++ source/CNC/CNC_Safety.cpp
@@ -5,7 +5,7 @@ extern CNC_Controler cnc;
 void CNC_Controler::endstop_callback(uint gpio, uint32_t events){
 
     cnc.hard_stop();
-    cnc.endstop_hit = true;
+    cnc.endstop_hit = true; // Alle Achsen verlieren Reference und Stoppen
     cnc.x_axis.dereference();
     cnc.y_axis.dereference();
     cnc.z_axis.dereference();
@@ -16,8 +16,8 @@ void CNC_Controler::endstop_check(bool on_off){
 
     this->endstop_check_active = on_off;
 
-    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
-    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_plus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
+    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback); // Interupts die Regelmäßig die Endschalter ÜBerprüfen
+    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_plus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback); // 
     gpio_set_irq_enabled_with_callback(this->y_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
     gpio_set_irq_enabled_with_callback(this->y_axis.get_endstop_pin_plus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
     gpio_set_irq_enabled_with_callback(this->z_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
