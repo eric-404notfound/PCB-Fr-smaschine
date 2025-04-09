12ec3c4c9e1506523bef092a0dde64e32ba6c4c1
Sallwuerk <marcleuze@outlook.de>
2025-04-09 11:48:43 +0200

    Update CNC_Safety.cpp
    Hinzufügen von Kommentaren

--- source/CNC/CNC_Safety.cpp
+++ source/CNC/CNC_Safety.cpp
@@ -4,26 +4,26 @@ extern CNC_Controler cnc;
 
 void CNC_Controler::endstop_callback(uint gpio, uint32_t events){
 
-    cnc.hard_stop();
-    cnc.endstop_hit = true; // Alle Achsen verlieren Reference und Stoppen
-    cnc.x_axis.dereference();
-    cnc.y_axis.dereference();
-    cnc.z_axis.dereference();
+    cnc.hard_stop(); // CNC hält sofort alles an, auch die Spindel
+    cnc.endstop_hit = true; // Alle Achsen verlieren Referenz und Stoppen
+    cnc.x_axis.dereference(); // Löschen der Referenz der X-Achse
+    cnc.y_axis.dereference(); // Löschen der Referenz der Y-Achse
+    cnc.z_axis.dereference(); // Löschen der Referenz der Z-Achse
     
 }
 
 void CNC_Controler::endstop_check(bool on_off){
 
-    this->endstop_check_active = on_off;
+    this->endstop_check_active = on_off; // Einschalten der Überwachung der Endschalter
 
-    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback); // Interupts die Regelmäßig die Endschalter ÜBerprüfen
-    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_plus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback); // 
+    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback); // Interupts die dauerhaft die Endschalter Überprüfen
+    gpio_set_irq_enabled_with_callback(this->x_axis.get_endstop_pin_plus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback); 
     gpio_set_irq_enabled_with_callback(this->y_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
     gpio_set_irq_enabled_with_callback(this->y_axis.get_endstop_pin_plus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
     gpio_set_irq_enabled_with_callback(this->z_axis.get_endstop_pin_minus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
     gpio_set_irq_enabled_with_callback(this->z_axis.get_endstop_pin_plus(), GPIO_IRQ_EDGE_FALL, on_off, &this->endstop_callback);
 
-    this->endstop_hit = false;
+    this->endstop_hit = false; // Wenn kein Endschalter betätigt wird läuft das Programm normal weiter
 
 }
 
